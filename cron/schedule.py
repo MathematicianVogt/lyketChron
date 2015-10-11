ddcd31ca4bdceb05139b27241c969bcad5fef916
Emma Foster <deafjams@gmail.com>
2015-10-11 13:20:58 -0400

    Adjusted cron to work with current codebase

--- cron/schedule.py
+++ cron/schedule.py
@@ -2,9 +2,9 @@ from plan import Plan
 from os import getcwd
 
 WORKING_DIR = getcwd() + '/../'
-cron = Plan("lyke_cron")
+cron = Plan("lyket_ingestion_cron")
 
-cron.script('example.py', every='1.minute', path=WORKING_DIR)
+cron.script('LyketJob.py', every='1.minute', path=WORKING_DIR)
 
 if __name__ == '__main__':
 	cron.run('update')
