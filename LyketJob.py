381c7ce50bcf1b9de148c8e49b90e3a82302b133
MathematicianVogt <MathematicianVogt@Ryans-MacBook-Pro.local>
2015-10-11 16:33:16 -0400

    did some optimization and finished DB
    Cron job + ability to add things to mongo database with thread pool.

--- LyketJob.py
+++ LyketJob.py
@@ -3,6 +3,7 @@ import time
 import thread
 import json
 from MongoLib import *
+from multiprocessing.dummy import Pool as ThreadPool 
 
 from NewsArticle import NewsArticle
 class LyketJob:
@@ -11,22 +12,60 @@ class LyketJob:
 		self.db=MongoLib("Lyket", "Articles")
 
 	#thread function, will put enteries into DB in paraell. Will build json, then put into DB
-	def put_article_in_db(self,article_tuple):
-		new_entry = {}
-		new_entry['art_sum']=article_tuple[0]
-		new_entry['art_auth']=article_tuple[1]
-		new_entry['art_thumb'] = article_tuple[2]
-		new_entry['art_pub'] = article_tuple[3]
-		new_entry['art_key_words'] = article_tuple[4]
-		new_entry['art_vids']  = article_tuple[5]
-		new_entry['likes']=0
-		new_entry['dislikes']=0
-		new_entry['comments'] = []
+	def put_article_in_db(self,story_url):
+		current_article = NewsArticle(story_url)
+		
+
+		
+		#publish date for article : datetime object 
+		article_published = current_article.date_made()
+		
+		
+
+		#title of article : String
+		article_title=current_article.get_title()
 
 
 
-		self.db.CollectionSubmitOne(new_entry)
-		print str(article_tuple[2])
+		
+
+
+
+		if(list(self.db.CollectionFind({'art_title': article_title, 'art_pub': article_published}))):
+			print "entry already found in DB title: " + article_title 
+		else:
+			current_article.goodArticle()
+			#keywords in article: Array of Strings
+			article_key_words = current_article.getKeywords()
+			
+			#videos in story : Array of Strings (url to videos)
+			article_videos = current_article.get_videos()
+
+
+			#summary of article : String
+			article_summary = current_article.getSummary()
+			
+			#authors of article: Array of Strings
+			article_authors = current_article.getAuthors()
+			
+			#image for article : String (url to image)
+			article_thumbnaillink = current_article.thumbnail_url()
+
+
+
+			new_entry = {}
+			new_entry['art_title']=article_title
+			new_entry['art_sum']=article_summary
+			new_entry['art_auth']=article_authors
+			new_entry['art_thumb'] = article_thumbnaillink
+			new_entry['art_pub'] = article_published
+			new_entry['art_key_words'] = article_key_words
+			new_entry['art_vids']  = article_videos
+			new_entry['likes']=0
+			new_entry['dislikes']=0
+			new_entry['comments'] = []
+			self.db.CollectionSubmitOne(new_entry)
+		
 
 
 
@@ -34,34 +73,16 @@ class LyketJob:
 
 	def runJob(self):
 		try:
+			pool = ThreadPool(8)
+			url_array=[]
 			for stream in self.file:
 
 				currentstream = feedparser.parse(stream)
 				for entry in currentstream['entries']:
 					story_url = entry['link']
-					current_article = NewsArticle(story_url)
+					url_array.append(story_url)
 					
-
-					#summary of article : String
-					article_summary = current_article.getSummary()
-					
-					#authors of article: Array of Strings
-					article_authors = current_article.getAuthors()
-					
-					#image for article : String (url to image)
-					article_thumbnaillink = current_article.thumbnail_url()
-					
-					#publish date for article : datetime object 
-					article_published = current_article.date_made()
-					
-					#keywords in article: Array of Strings
-					article_key_words = current_article.getKeywords()
-					
-					#videos in story : Array of Strings (url to videos)
-					article_videos = current_article.get_videos()
-					
-					#make json out of these parameters and put into db.
-					thread.start_new_thread(self.put_article_in_db, ((article_summary,article_authors,article_thumbnaillink,article_published, article_key_words,article_videos), ))
+				result = pool.map(self.put_article_in_db, url_array)
 		except Exception as e:
 			print "The following issue occured: "
 			print e
--- NewsArticle.py
+++ NewsArticle.py
@@ -1,12 +1,15 @@
 import newspaper
 from newspaper import Article
+import datetime
 class NewsArticle:
 	def __init__(self,url):
 		self.article = Article(url)
 		self.article.download()
 		self.article.parse()
-		self.article.nlp()
 	
+	
+	def goodArticle(self):
+		self.article.nlp()
 	def getKeywords(self):
 		x = self.article.keywords
 		for i in range(0,len(x)):
@@ -28,9 +31,12 @@ class NewsArticle:
 		return self.article.top_image.encode('ascii', 'ignore')
 	
 	def date_made(self):
+		
 		return self.article.publish_date
 	def get_videos(self):
 		x=self.article.movies
 		for i in range(0,len(x)):
 			x[i] = x[i].encode('ascii', 'ignore')
 		return x
+	def get_title(self):
+		return self.article.title.encode('ascii','ignore')
