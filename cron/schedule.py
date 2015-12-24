9cc467d38ec97bc776d7c2329013e69f6b3ac9a2
Emma Foster <deafjams@gmail.com>
2015-12-23 20:57:54 -0500

    whitespace fix

--- cron/schedule.py
+++ cron/schedule.py
@@ -7,7 +7,7 @@ cron = Plan("lyket_ingestion_cron")
 cron.script('LyketJob.py', every='5.minutes', path=WORKING_DIR)
 
 if __name__ == '__main__':
-	try:
+    try:
         cron.run('update')
     except:
         cron.run('write')
